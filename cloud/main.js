9c78bda6ef0e709205185899472ceea7c8b6485a
kennyNing <617877410@qq.com>
2015-05-20 11:44:37 +0800

    kenny up
    1123

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,6 @@
 var data_obj = AV.Object.extend("pages_data");
 var arr = [1, 2, 3];
 var testme = [];
-var abc;
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
